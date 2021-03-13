7aeac1a3987e7d9290cd11c1fc356997bfdd28b0
magbone <2300427529@qq.com>
2021-03-13 19:47:32 +0800

    Added: Unix support

--- syn_attack.h
+++ syn_attack.h
@@ -1,8 +1,10 @@
-#ifndef SYN_ATTACK_H
+#ifndef _SYN_ATTACK_H_
+#define _SYN_ATTACK_H_
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -12,21 +14,33 @@
 #include <arpa/inet.h>
 #include <errno.h>
 
-// source port
-#define SRC_PORT 30902
 
 // packet size
-#define P_SIZE 512
+#define P_SIZE (sizeof(struct ip) + sizeof(struct tcphdr))
+
+#define WIN_SIZE 65535
 
 
 
+struct tcp_checksum_hdr
+{
+      struct in_addr ip_src, ip_dst;
+      u_char zero;
+      u_char ip_p;
+      u_short tcp_len; 
+      struct tcphdr tcp_hdr;
+};
 
 int syn_attack_sock(const char *, const char *, u_int16_t);
 
-static u_int16_t packet_checksum(u_int16_t *, size_t);
+static u_int16_t tcp_checksum(struct ip *, struct tcphdr *);
 
-static struct ip* ipv4_header_create(const char *, const char *, int);
+static struct ip* ipv4_hdr_create(const char *, const char *, int);
 
-static struct tcphdr* tcphdr_create(const u_int16_t);
+static struct tcphdr* tcp_hdr_create(const u_int16_t, struct ip *);
 
-#endif // !SYN_ATTACK_H
+static u_int32_t generate_random_seq();
+
+static u_int16_t generate_random_port();
+
+#endif // !_SYN_ATTACK_H_
